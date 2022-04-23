98df6b38e5cf0d37fa413c113275419986c5d101
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2022-04-23 18:03:44 +0500

    Updated

---
+++ neumorphism_ui/lib/widgets/neu-btn.widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class NeuButton extends StatefulWidget {
+  const NeuButton({Key? key}) : super(key: key);
+
+  @override
+  State<NeuButton> createState() => _NeuButtonState();
+}
+
+class _NeuButtonState extends State<NeuButton> {
+  bool _isElevated = false;
+  //
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            _isElevated = !_isElevated;
+          },
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 200),
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(50),
+                boxShadow: _isElevated
+                    ? [
+                        const BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(4, 4),
+                          blurRadius: 15,
+                          spreadRadius: 1,
+                        ),
+                        const BoxShadow(
+                          color: Colors.white,
+                          offset: Offset(-4, -4),
+                          blurRadius: 15,
+                          spreadRadius: 1,
+                        ),
+                      ]
+                    : null),
+          ),
+        ),
+      ),
+    );
+  }
+}
